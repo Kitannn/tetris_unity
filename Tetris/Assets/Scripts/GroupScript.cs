e2246c4e76fc2b99fb9b930e87675b90019f0918
kitannn <hkitannn@gmail.com>
2019-03-25 00:21:49 -0700

    broken

---
+++ Tetris/Assets/Scripts/GroupScript.cs
@@ -0,0 +1,107 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GroupScript : MonoBehaviour
+{
+    public float lastFall = 0;
+
+    bool isValidGridPos()
+    {
+        foreach (Transform child in transform)
+        {
+            Vector2 v = Grid.roundVec2(child.position);
+
+            if (!Grid.insideBorder(v))
+                return false;
+
+            if (Grid.grid[(int)v.x, (int)v.y] != null && Grid.grid[(int)v.x, (int)v.y].parent != transform)
+                return false;
+        }
+        return true;
+    }
+
+    void updateGrid()
+    {
+        for (int y = 0; y < Grid.h; ++y)
+            for (int x = 0; x < Grid.w; ++x)
+                if (Grid.grid[x,y] != null)
+                    if (Grid.grid[x, y]. parent == transform)
+                        Grid.grid[x, y] = null;
+
+        foreach (Transform child in transform)
+        {
+            Vector2 v = Grid.roundVec2(child.position);
+            Grid.grid[(int)v.x, (int)v.y] = child;
+        }
+    }
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        if (!isValidGridPos())
+        {
+            Debug.Log("GAME OVER");
+            Destroy(gameObject);
+        }
+    }
+    // Update is called once per frame
+    void Update()
+    {
+        //left
+        if (Input.GetKeyDown(KeyCode.LeftArrow))
+        {
+            transform.position += new Vector3(-1, 0, 0);
+
+            //check valid
+            if (isValidGridPos())
+                updateGrid();
+            else
+                transform.position += new Vector3(1, 0, 0);
+        }
+
+        //right
+        else if (Input.GetKeyDown(KeyCode.RightArrow))
+        {
+            transform.position += new Vector3(1, 0, 0);
+
+            if (isValidGridPos())
+                updateGrid();
+            else
+                transform.position += new Vector3(-1, 0, 0);
+        }
+
+        //rotate
+        else if (Input.GetKeyDown(KeyCode.UpArrow))
+        {
+            transform.Rotate(0, 0, -90);
+
+            if (isValidGridPos())
+                updateGrid();
+            else
+                transform.Rotate(0, 0, 90);
+        }
+
+        //down
+        else if (Input.GetKeyDown(KeyCode.DownArrow) || Time.time - lastFall >= 1)
+        {
+            transform.position += new Vector3(0, -1, 0);
+
+            if (isValidGridPos())
+                updateGrid();
+            else
+            {
+                transform.position += new Vector3(0, 1, 0);
+
+                //clear filled
+                Grid.deleteFullRows();
+                //next piece
+                FindObjectOfType<SpawnerScript>().spawnNext();
+                //disable movement
+                enabled = false;
+            }
+
+            lastFall = Time.time;
+        }
+    }
+}
---
+++ Tetris/Assets/Scripts/SpawnerScript.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SpawnerScript : MonoBehaviour
+{
+    public GameObject[] groups;
+    public int i;
+
+    public GameObject createGroup(Vector3 v)
+    {
+        GameObject group = Instantiate(groups[i], transform.position, Quaternion.identity);
+        return group;
+    }
+
+    public void spawnNext()
+    {
+        createGroup(transform.position);
+        i = Random.Range(0, groups.Length);
+    }
+    // Start is called before the first frame update
+    void Start()
+    {
+        spawnNext();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
